aa7ea7c2280a8926ebf44b8845bd6016f0533564
natelloyd1 <nathanaellloyd@live.com>
2020-07-11 16:00:25 +0100

    fixed form submissions

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -1,46 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const encode = (data) => {
-  return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-}
+// const encode = (data) => {
+//   return Object.keys(data)
+//       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+//       .join("&");
+// }
 
 function Contact() {
 
-    const [name, setName] = useState('');
-    const handleName = e => {
-      setName(e.currentTarget.value);
-      };
+  //   const [name, setName] = useState('');
+  //   const handleName = e => {
+  //     setName(e.currentTarget.value);
+  //     };
     
-    const [subject, setSubject] = useState('');
-    const handleSubject = e => {
-        setSubject(e.currentTarget.value); 
-    }
+  //   const [subject, setSubject] = useState('');
+  //   const handleSubject = e => {
+  //       setSubject(e.currentTarget.value); 
+  //   }
 
-	const [email, setEmail] = useState('');
-	const handleEmail = e => {
-		setEmail(e.currentTarget.value);
-	};
+	// const [email, setEmail] = useState('');
+	// const handleEmail = e => {
+	// 	setEmail(e.currentTarget.value);
+	// };
 
-	const [message, setMessage] = useState('');
-	const handleMessage = e => {
-		setMessage(e.currentTarget.value);
-  };
+	// const [message, setMessage] = useState('');
+	// const handleMessage = e => {
+	// 	setMessage(e.currentTarget.value);
+  // };
   
-  const sendEmail = e => {
+  // const sendEmail = e => {
     
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...useState })
-    }) 
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: encode({ "form-name": "contact", ...useState })
+  //   }) 
+  //     .then(() => alert("Success!"))
+  //     .catch(error => alert(error));
 
-    e.preventDefault();
-    console.log((name), (email), (message)); 
-  };
+  //   e.preventDefault();
+  //   console.log((name), (email), (message)); 
+  // };
 
 
   return (
@@ -59,54 +59,63 @@ function Contact() {
 
     <div className="form-wrap">
 
-    <form onSubmit={ sendEmail } name="contact" action="/" method="POST" data-netlify="true" >
+    <form name="contact" action="/" method="POST" data-netlify="true" >
+    <input type='hidden' name='form-name' value='contact' />
 
-        <div className="input-wrap">
-
-			<legend>Name</legend>
+      
+      <div className="input-wrap">
+			<label htmlFor="yourname">
+        Name
+      </label>
           <input
-            value={ name }
             type='text'
             name='name'
-            onChange={ handleName }
+            id="yourname"
           />
         </div>
 
         <div className="input-wrap">   
-          <legend>Email</legend>
+        <label htmlFor="youremail">
+        Email
+        </label>
             <input
-              value={ email }
               type='email'
               name='email'
-              onChange={ handleEmail }
+              id="youremail"
             />
         </div>
 
         <div className="input-wrap">  
-          <legend>Subject</legend>
+        <label htmlFor="yoursubject">
+        Subject
+        </label>
             <input
-              value={ subject }
               type='subject'
               name='subject'
-              onChange={ handleSubject }
+              id="yoursubject"
             />
         </div>
             
         <div className="input-wrap">  
-          <legend>Message</legend>
+        <label htmlFor="yourmessage">
+        Message
+        </label>
             <textarea
               className="msg-box"
-              value={ message }
               name='message'
-              onChange={ handleMessage }
+              id="yourmessage"
+
 				/>
         </div>
 
-			<input className='input-button' type='submit' value="Send" />
+        <button type="submit" className="input-button">Send</button>
+
+			{/* <input className='input-button' type='submit' value="Send" /> */}
 
 		  </form>
 
-        </div>
+
+      </div>
 
         </>
         
